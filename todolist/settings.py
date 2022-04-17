0e7a7483e970124a80a64d5bd69fe84069925100
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-04-17 20:45:13 +0300

    prepared for deploy v32

--- todolist/settings.py
+++ todolist/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env("SECRET_KEY", 'django-insecure-qsib7!68w^zbce6)$jf3hyyul0f*j1do91$4gx)v@#bbcs+#7g')
-SECRET_KEY = 'django-insecure-qsib7!68w^zbce6)$jf3hyyul0f*j1do91$4gx)v@#bbcs+#7g'
+SECRET_KEY = env("SECRET_KEY")
+# SECRET_KEY = 'django-insecure-qsib7!68w^zbce6)$jf3hyyul0f*j1do91$4gx)v@#bbcs+#7g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", False)
