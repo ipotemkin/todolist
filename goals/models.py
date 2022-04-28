a3e131489789233ac2104171f704d92652ebbc8a
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-04-28 23:37:33 +0300

    added auto_now/auto_now_add + Satus/Priority

--- goals/models.py
+++ goals/models.py
@@ -4,6 +4,20 @@ from django.utils import timezone
 from core.models import User
 
 
+class Status(models.IntegerChoices):
+    to_do = 1, "К выполнению"
+    in_progress = 2, "В процессе"
+    done = 3, "Выполнено"
+    archived = 4, "Архив"
+
+
+class Priority(models.IntegerChoices):
+    low = 1, "Низкий"
+    medium = 2, "Средний"
+    high = 3, "Высокий"
+    critical = 4, "Критический"
+
+
 class GoalCategory(models.Model):
     class Meta:
         verbose_name = "Категория"
@@ -12,11 +26,22 @@ class GoalCategory(models.Model):
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
     is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
-    created = models.DateTimeField(verbose_name="Дата создания")
-    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
+    created = models.DateTimeField(verbose_name="Дата создания", auto_now_add=True)
+    updated = models.DateTimeField(verbose_name="Дата последнего обновления", auto_now=True)
 
-    def save(self, *args, **kwargs):
-        if not self.id:  # Когда объект только создается, у него еще нет id
-            self.created = timezone.now()  # проставляем дату создания
-        self.updated = timezone.now()  # проставляем дату обновления
-        return super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     if not self.id:  # Когда объект только создается, у него еще нет id
+    #         self.created = timezone.now()  # проставляем дату создания
+    #     self.updated = timezone.now()  # проставляем дату обновления
+    #     return super().save(*args, **kwargs)
+
+
+# class Goal(models.Model):
+#     title = models.CharField(verbose_name="Название", max_length=255)
+#     description = models.CharField(verbose_name="Описание", max_length=255)
+#     category = models.ForeignKey(GoalCategory, verbose_name="Категория", on_delete=models.PROTECT)
+#     # status =
+#     # priority =
+#     deadline = models.DateTimeField(verbose_name="Дедлайн")
+#     created = models.DateTimeField(verbose_name="Дата создания")
+#     updated = models.DateTimeField(verbose_name="Дата последнего обновления")
