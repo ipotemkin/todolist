3cfcf4606ee306c7adfb7654d335e48ea6106e1c
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-05-01 19:13:40 +0300

    added goals_board&boardparticipant

--- goals/admin.py
+++ goals/admin.py
@@ -1,23 +1,42 @@
 from django.contrib import admin
 
-from goals.models import GoalCategory, Goal, Comment
+from goals.models import GoalCategory, Goal, Comment, Board, BoardParticipant
 
 
 @admin.register(GoalCategory)
 class GoalCategoryAdmin(admin.ModelAdmin):
-    list_display = ("title", "user", "created", "updated")
+    list_display = ("title", "board", "user", "created", "updated")
     search_fields = ("title", "user")
     list_filter = ('is_deleted',)
+    readonly_fields = ("created", "updated")
 
 
 @admin.register(Goal)
 class GoalAdmin(admin.ModelAdmin):
-    list_display = ("category", "title", "description", "due_date", "created", "updated")
+    list_display = ("title", "category", "description", "due_date", "created", "updated")
     search_fields = ("title", "description")
     list_filter = ('is_deleted',)
+    readonly_fields = ("created", "updated")
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ("goal", "text", "created", "updated")
     search_fields = ("text", "goal__title")
+    readonly_fields = ("created", "updated")
+
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("title", "created", "updated")
+    search_fields = ("title",)
+    list_filter = ('is_deleted',)
+    readonly_fields = ("created", "updated")
+
+
+@admin.register(BoardParticipant)
+class BoardParticipantAdmin(admin.ModelAdmin):
+    list_display = ("board", "user", "role", "created", "updated")
+    search_fields = ("board",)
+    # list_filter = ('is_deleted',)
+    readonly_fields = ("created", "updated")
--- goals/models.py
+++ goals/models.py
@@ -11,20 +11,30 @@ class DatesModelMixin(models.Model):
     updated = models.DateTimeField(verbose_name="Дата последнего обновления", auto_now=True)
 
 
-class GoalCategory(DatesModelMixin):
+class FieldIsDeletedMixin(models.Model):
+    class Meta:
+        abstract = True
+
+    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+
+
+class GoalCategory(DatesModelMixin, FieldIsDeletedMixin):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
 
     title = models.CharField(verbose_name="Название", max_length=255)
     user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
-    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+    # is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+    board = models.ForeignKey(
+        'Board', verbose_name="Доска", on_delete=models.PROTECT, related_name="categories", null=True
+    )
 
     def __str__(self):
         return self.title
 
 
-class Goal(DatesModelMixin):
+class Goal(DatesModelMixin, FieldIsDeletedMixin):
     class Meta:
         verbose_name = "Цель"
         verbose_name_plural = "Цели"
@@ -44,7 +54,7 @@ class Goal(DatesModelMixin):
     title = models.CharField(verbose_name="Название", max_length=255)
     description = models.CharField(verbose_name="Описание", max_length=255, null=True, blank=True)
     # user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
-    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+    # is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
     category = models.ForeignKey(GoalCategory, verbose_name="Категория", on_delete=models.PROTECT)
     status = models.PositiveSmallIntegerField(
         verbose_name="Статус",
@@ -72,3 +82,43 @@ class Comment(DatesModelMixin):
 
     def __str__(self):
         return self.text[:30]
+
+
+class Board(DatesModelMixin, FieldIsDeletedMixin):
+    class Meta:
+        verbose_name = "Доска"
+        verbose_name_plural = "Доски"
+
+    title = models.CharField(verbose_name="Название", max_length=255)
+    # is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class BoardParticipant(DatesModelMixin):
+    class Meta:
+        unique_together = ("board", "user")
+        verbose_name = "Участник"
+        verbose_name_plural = "Участники"
+
+    class Role(models.IntegerChoices):
+        owner = 1, "Владелец"
+        writer = 2, "Редактор"
+        reader = 3, "Читатель"
+
+    board = models.ForeignKey(
+        Board,
+        verbose_name="Доска",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    user = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    role = models.PositiveSmallIntegerField(
+        verbose_name="Роль", choices=Role.choices, default=Role.owner
+    )
