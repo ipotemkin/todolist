3171586eba7131e233114ea5ea18fb5c4c439d85
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2022-05-01 19:40:47 +0300

    added goals.serializers.BoardCreateSerializer

--- goals/serializers.py
+++ goals/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.serializers import ValidationError
 
 from core.serializers import UserSerializer
-from goals.models import GoalCategory, Goal, Comment
+from goals.models import GoalCategory, Goal, Comment, Board, BoardParticipant
 
 
 class GoalCategoryCreateSerializer(serializers.ModelSerializer):
@@ -71,3 +71,20 @@ class CommentSerializer(CommentCreateSerializer):
 
     class Meta(CommentCreateSerializer.Meta):
         read_only_fields = ("id", "created", "updated", "goal")
+
+
+class BoardCreateSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = Board
+        read_only_fields = ("id", "created", "updated")
+        fields = "__all__"
+
+    def create(self, validated_data):
+        user = validated_data.pop("user")
+        board = Board.objects.create(**validated_data)
+        BoardParticipant.objects.create(
+            user=user, board=board, role=BoardParticipant.Role.owner
+        )
+        return board
